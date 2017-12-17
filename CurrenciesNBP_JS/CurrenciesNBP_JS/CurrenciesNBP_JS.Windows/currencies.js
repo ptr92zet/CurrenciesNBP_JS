6c91248585894fef8288cfa54067fa8be9a9c559
Piotr Zietek <zietekpiotr5@gmail.com>
2017-12-17 15:00:17 +0100

    Finally managed to download all data. Needs tuning (no scrolling) and more

--- CurrenciesNBP_JS/CurrenciesNBP_JS/CurrenciesNBP_JS.Windows/currencies.js
+++ CurrenciesNBP_JS/CurrenciesNBP_JS/CurrenciesNBP_JS.Windows/currencies.js
@@ -9,26 +9,24 @@
     var templateTable;
     var ratingEntriesTable;
     var currencyTemplate;
-    var ratingsData;
-
-    var codes = [];
-    var names = [];
-    var ratings = [];
+    var ratingsData = [];
 
     var years = [];
     var currentYear = (new Date()).getFullYear();
     var initialYear = 2002;
 
+    var dateFiles = [];
+    var dates = [];
+
+    var codes = [];
+    var names = [];
+    var ratings = [];
+
     WinJS.UI.Pages.define("default.html", {
 
         ready: function () {
-            console.log("St-arting");
-            console.log("Loading document elements");
             this.initDocumentElements();
-            console.log("Loaded document elements");
-            console.log("Initializing years");
             this.initYears();
-            console.log("Initialized years");
         },
 
         initDocumentElements: function () {
@@ -37,7 +35,6 @@
             selectYearList = document.getElementById("selectYear");
             selectDateList = document.getElementById("selectDate");
             ratingEntriesTable = document.getElementById("ratingEntries");
-            currencyTemplate = document.getElementById("currencyTemplate");
         },
 
         initYears: function () {
@@ -58,14 +55,97 @@
                 selectYearList.appendChild(option);
             }
 
-            selectYearList.onchange = getDates;
+            selectYearList.onchange = this.yearSelected;
 
             progressBar.style.visibility = "hidden";
         },
-    })
-    
-    function getDates() {
 
-    }
+        yearSelected: function () {
+            selectDateList.options.length = 0;
+            var selectedYear = selectYearList.value;
+            var httpRequest = new XMLHttpRequest();
+
+            if (selectedYear == currentYear) {
+                httpRequest.open("GET", 'http://www.nbp.pl/kursy/xml/dir.txt');
+            }
+            else {
+                httpRequest.open("GET", 'http://www.nbp.pl/kursy/xml/dir' + selectedYear + '.txt');
+            }
+
+            httpRequest.onreadystatechange = function () {
+                if (httpRequest.readyState == 4) {
+                    document.getElementById("progressBar").style.visibility = "visible";
+                    var response = httpRequest.responseText;
+                    var processedResponse = response.split("\n");
+                    for (var i = 0; i < processedResponse.length - 1; i++) {
+                        if (!(processedResponse[i].substring(0, 1) === "a")) {
+                            continue;
+                        }
+                        var dateStr = "20" + processedResponse[i].substring(5, 7) + "-" + processedResponse[i].substring(7, 9) + "-" + processedResponse[i].substring(9, 11);
+                        dates[dates.length] = dateStr;
+                        dateFiles[dateFiles.length] = processedResponse[i];
+                    }
+
+                    for (var i = 0; i < dates.length; i++) {
+                        var option = document.createElement("option");
+                        option.text = dates[i];
+                        option.value = dates[i];
+                        selectDateList.appendChild(option);
+                    }
+                    document.getElementById("progressBar").style.visibility = "hidden";
+
+                    selectDateList.onchange = function () {
+                        progressBar.style.visibility = "visible";
+                        var index = selectDateList.selectedIndex;
+                        var selectedDate = selectDateList.value;
+                        var dateFile = dateFiles[index];
+                        var dat = dates[index];
+                        console.log("Selected date: " + selectedDate + ", index: " + index + ", datesIndex: " + dat + ", dateFilesIndex: " + dateFile);
+                        var xmlRequest = new XMLHttpRequest();
+                        var url = 'http://api.nbp.pl/api/exchangerates/tables/a/' + selectedDate + '?format=xml';
+                        console.log("URL: " + url);
+                        xmlRequest.open("GET", url);
+                        xmlRequest.onreadystatechange = function () {
+                            if (xmlRequest.readyState == 4) {
+                                var xml = xmlRequest.responseXML;
+                                var rateElements = xml.getElementsByTagName("Rate");
+                                for (var i = 0; i < rateElements.length; i++) {
+                                    codes[i] = rateElements[i].getElementsByTagName("Code")[0].childNodes[0].nodeValue;
+                                    names[i] = rateElements[i].getElementsByTagName("Currency")[0].childNodes[0].nodeValue;
+                                    ratings[i] = rateElements[i].getElementsByTagName("Mid")[0].childNodes[0].nodeValue;
+                                }
+                                setTimeout(function () {
+                                    progressBar.style.visibility = "visible";
+                                    ratingsData.length = 0;
+                                    var rows = ratingEntriesTable.getElementsByTagName('tr');
+                                    var rowsCount = rows.length;
+
+                                    for (var i = rowsCount - 1; i >= 0; i--) {
+                                        ratingEntriesTable.removeChild(rows[i]);
+                                    }
+
+                                    var templateControl = document.getElementById("currencyTemplate").winControl;
+                                    for (var i = 0; i < codes.length; i++) {
+                                        ratingsData.push({ code: codes[i], name: names[i], rating: ratings[i] });
+                                    }
+                                    ratingsData.forEach(function (rating) {
+                                        templateControl.render(rating, ratingEntriesTable);
+                                    })
+                                    templateTable.style.visibility = "visible";
+                                    progressBar.style.visibility = "hidden";
+                                }, 2000);
+                            }
+                        }
+                        xmlRequest.send();
+                    }
+                }
+            }
+            httpRequest.send();
+        },
+
+        getAllRatingsFromDate: function () {
+
+        }
+    });
 
 }());
